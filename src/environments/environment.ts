3ad3c134b800403758c86ada1269d7fe0d02b6ac
enjelliabarce <enjelliabarcepratama@mail.ugm.ac.id>
2024-09-08 23:25:09 +0700

    acara3

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDH1oJG78AYyGR_iGCn9pAhOytc_KvfRTE",
+    authDomain: "reciclica-784c0.firebaseapp.com",
+    projectId: "reciclica-784c0",
+    storageBucket: "reciclica-784c0.appspot.com",
+    messagingSenderId: "761034345502",
+    appId: "1:761034345502:web:7ef7164aa4fef2e0bbb20f"
+  }
+};
